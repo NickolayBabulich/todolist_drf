000e1d5880accbbcdd45d2ed9471490cd8e69ca4
Nickolai Babulich <nickolas.grn@yandex.ru>
2023-12-25 12:48:36 +1200

    add views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView, DestroyAPIView
+from users.models import User
+from users.serializers import UserSerializer, UserDetailSerializer, UserUpdateSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserDetailSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(serializer.data['password'])
+        user.save()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    serializer_class = UserDetailSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
