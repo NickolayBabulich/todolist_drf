08cee84f1afc95f239f2fb74e37939669e4013b7
Nickolai Babulich <nickolas.grn@yandex.ru>
2023-12-25 12:48:15 +1200

    add model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=15)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
